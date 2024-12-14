60f4d191485f18826e4f05cd7d75efa1dceec4f7
Samartha Bhat <samarthabhat11@gmail.com>
2024-12-14 19:07:01 +0530

    Update main.go
    Nested struct

--- Structs/main.go
+++ Structs/main.go
@@ -9,6 +9,18 @@ type Person struct{
 	Phone string
 }
 
+// Structs with Nested Fields
+
+type Address struct {
+	City string
+	Zipcode  string
+}
+
+type Human struct{
+	Name string
+	Address Address
+}
+
 func main(){
 
 person1 := Person{
@@ -22,6 +34,7 @@ person1 := Person{
 person2 := Person{Name: "Alice", Gender: "Female"}
 
 
+
 // Access fields
 
 fmt.Println("Person 1 :", person1)
@@ -31,6 +44,37 @@ fmt.Printf("Name: %s, Phone: %s\n", person1.Name, person1.Phone)
     person2.Phone = "6363271234"
     fmt.Printf("Person 2: %+v\n", person2)
 
+	//display the person
+	fmt.Println("Before update:")
+	displayPerson(person1)
+	
+	//Update the phone number
+	updatePhone(&person1, "8237652331")
+
+	fmt.Println("After Update:")
+	// Display the updated person
+	displayPerson(person1)
+
+
+	// Usage for nested
+	person := Human{
+		Name: "Ron",
+		Address: Address{
+			City: "New York",
+			Zipcode: "10001",
+		},
+	}
+	fmt.Printf(" Nested struct \n City: %s, Zipcode: %s\n", person.Address.City, person.Address.Zipcode)
+}
+
+// Pass by value
+
+func displayPerson(p Person){
+	fmt.Printf("Name: %s, Age: %d, Phone:%s\n", p.Name, p.Age, p.Phone)
+}
+
+func updatePhone(p *Person, newPhone string){
+	p.Phone = newPhone // Modify the field via pointer
 }
 
 
