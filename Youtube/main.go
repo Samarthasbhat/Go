20654fd426131e2fb29f9a984eabd563c6d76e5a
Samartha Bhat <samarthabhat11@gmail.com>
2025-03-20 20:00:52 +0530

    commit

--- Youtube/main.go
+++ Youtube/main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"youtube/hello" // Importing the package correctly
+	"os"  // required for the stdin and stderr
+	// "youtube/hello" // Importing the package correctly
 )
 
 func main() {
 
-		fmt.Println(hello.Say(os.Args[1:])) // Calling the Say function
 
+	// fmt.Println(hello.Say(os.Args[1:])) // Calling the Say function
+
+	var sum float64
+	var n int
+
+	for {
+		var val float64
+
+		 _, err := fmt.Fscanln(os.Stdin, &val)
+			if err != nil{
+				break
+		}
+		sum  += val
+		n++
+	}
+
+	if n == 0{
+		fmt.Fprintln(os.Stderr, "no values")
+		os.Exit(-1)
+	}
+	fmt.Println("The average is", sum/float64(n))
 }
