552aeba8ba0479782432278c5a56eb85cf65bc85
Samartha Bhat <samarthabhat11@gmail.com>
2025-05-02 21:50:25 +0530

    commit

--- Go Concurrency/Select/main.go
+++ Go Concurrency/Select/main.go
@@ -5,6 +5,7 @@ package main
 import (
 	"fmt"
 	"time"
+
 )
 
 
@@ -28,7 +29,14 @@ c1Count, c2Count := RunSelectExample()
 	fmt.Printf("c1Count: %d\n", c1Count)
 	fmt.Printf("c2Count: %d\n", c2Count)
 
+
+	Permanent()
+
+	NotReady()
+
+	Report()
 }
+
 
 //  Multiple channels being ready simultaneously seems interesting.
 
@@ -47,4 +55,55 @@ func RunSelectExample() (int, int){
 		}
 	}
 	return c1Count, c2Count
+}
+
+
+//  What if there are never any channels that become ready? this is not permenant so we can use the time package
+
+func Permanent() {
+	var c<-chan int
+
+	select{ 
+	case <- c:  // This case statement will never become unblocked because we're reading from "nil" channel.
+	case <-time.After(1*time.Second):
+		fmt.Println("Timed out")
+	}
+}
+
+//  When no channel is ready
+
+func NotReady() {
+	start := time.Now()
+	var c1, c2 <-chan int 
+	select{
+	case <-c1:
+	case<-c2:
+	default:
+		fmt.Printf("In default after %v\n\n", time.Since(start))
+	}
+}
+
+// Work while waiting for another goroutine to report a result
+
+func Report(){
+	done := make(chan interface{})
+
+	go func ()  {
+		time.Sleep(5*time.Second)
+		close(done)
+	}()
+	workCounter  := 0
+	loop:
+	for {
+		select{
+		case <-done:
+			break loop
+		default:
+		}
+
+		// Simulate work
+		workCounter++
+		time.Sleep(1*time.Second)
+	}
+	fmt.Printf("Achieved %v cycles of work before signalled to stop. \n", workCounter)
 }
